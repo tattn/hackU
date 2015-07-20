67408f937dd857a320f2255b4569dbb7dd7aae4d
Tatsuya Tanaka <tatsuyars@yahoo.co.jp>
2015-07-20 12:51:51 +0900

    Add methods for borrow api to Backend class & Refactor Backend class

--- hackuProduct/Backend.h
+++ hackuProduct/Backend.h
@@ -19,24 +19,37 @@ typedef void (^CompletionBlock)(id responseObject, NSError *error);
 
 + (instancetype)shared;
 
+// Default parameters
+#define DEFAULT_PARAM  option:(NSDictionary*)option callback:(CompletionBlock)callback
+#define DEFAULT_PARAM2        (NSDictionary*)option callback:(CompletionBlock)callback
+
 // === [/books] Books API
-- (void)addBook:(NSString*)title option:(NSDictionary*)option callback:(CompletionBlock)callback;
-- (void)getBook:(int)bookId callback:(CompletionBlock)callback;
-- (void)updateBook:(int)bookId option:(NSDictionary*)option callback:(CompletionBlock)callback;
-- (void)deleteBook:(int)bookId option:(NSDictionary*)option callback:(CompletionBlock)callback;
-- (void)searchBook:(NSDictionary*)option callback:(CompletionBlock)callback;
+- (void)addBook:(NSString*)title DEFAULT_PARAM;
+- (void)getBook:(int)bookId DEFAULT_PARAM;
+- (void)updateBook:(int)bookId DEFAULT_PARAM;
+- (void)deleteBook:(int)bookId DEFAULT_PARAM;
+- (void)searchBook:DEFAULT_PARAM2;
 // === [/books] end
 
 // === [/bookshelves] Bookshelves API
-- (void)getBookshelf:(int)userId option:(NSDictionary*)option callback:(CompletionBlock)callback;
-- (void)addBookToBookshelf:(int)userId bookId:(int)bookId option:(NSDictionary*)option callback:(CompletionBlock)callback;
-- (void)updateBookshelf:(int)userId option:(NSDictionary*)option callback:(CompletionBlock)callback;
-- (void)deleteBookshelf:(int)userId option:(NSDictionary*)option callback:(CompletionBlock)callback;
-- (void)getBookInBookshelf:(int)userId bookId:(int)bookId option:(NSDictionary*)option callback:(CompletionBlock)callback;
-- (void)updateBookInBookshelf:(int)userId bookId:(int)bookId option:(NSDictionary*)option callback:(CompletionBlock)callback;
-- (void)deleteBookInBookshelf:(int)userId bookId:(int)bookId option:(NSDictionary*)option callback:(CompletionBlock)callback;
-- (void)searchBookInBookshelf:(int)userId option:(NSDictionary*)option callback:(CompletionBlock)callback;
+- (void)getBookshelf:(int)userId DEFAULT_PARAM;
+- (void)addBookToBookshelf:(int)userId bookId:(int)bookId DEFAULT_PARAM;
+- (void)updateBookshelf:(int)userId DEFAULT_PARAM;
+- (void)deleteBookshelf:(int)userId DEFAULT_PARAM;
+- (void)getBookInBookshelf:(int)userId bookId:(int)bookId DEFAULT_PARAM;
+- (void)updateBookInBookshelf:(int)userId bookId:(int)bookId DEFAULT_PARAM;
+- (void)deleteBookInBookshelf:(int)userId bookId:(int)bookId DEFAULT_PARAM;
+- (void)searchBookInBookshelf:(int)userId DEFAULT_PARAM;
 // === [/bookshelves] end
+
+// === [/users/:user_id/borrow] Borrow API
+- (void)getBorrow:(int)userId DEFAULT_PARAM;
+- (void)addBorrow:(int)userId bookId:(int)bookId lenderId:(int)lenderId DEFAULT_PARAM;
+- (void)deleteBorrow:(int)userId bookId:(int)bookId DEFAULT_PARAM;
+// === [/users/:user_id/borrow] end
+
+#undef DEFAULT_PARAM
+#undef DEFAULT_PARAM2
 
 @end
 
--- hackuProduct/Backend.m
+++ hackuProduct/Backend.m
@@ -57,36 +57,48 @@ failure:^(NSURLSessionDataTask *task, NSError *error) {\
 
 // URL builders
 #define MAKE_URL(fmt, ...) [NSString stringWithFormat:(fmt), __VA_ARGS__]
+#define USER_URL @"users"
 #define BOOK_URL @"books"
 #define BOOKID_URL(bookId) MAKE_URL(BOOK_URL "/%d", (bookId))
 #define BOOKSHELF_URL @"bookshelves"
 #define BOOKSHELFID_URL(userId) MAKE_URL(BOOKSHELF_URL "/%d", (userId))
 #define BOOKSHELFIDID_URL(userId, bookId) MAKE_URL(BOOKSHELF_URL "/%d/%d", (userId), (bookId))
+#define BORROW_URL(userId) MAKE_URL(USER_URL "/%d/borrow", (userId))
+#define BORROWID_URL(userId, bookId) MAKE_URL(USER_URL "/%d/borrow/%d", (userId), (bookId))
+
+// Default parameters
+#define DEFAULT_PARAM  option:(NSDictionary*)option callback:(CompletionBlock)callback
+#define DEFAULT_PARAM2        (NSDictionary*)option callback:(CompletionBlock)callback
+
+#define MAKE_PARAM(dict) \
+NSMutableDictionary *param = [(dict) mutableCopy];\
+[param addEntriesFromDictionary:option];
+
+// Cast utilities
+#define INT2NS(val) [NSNumber numberWithInt:(val)]
+
+
 
 // === [/books] Books API
 
-- (void)addBook:(NSString*)title option:(NSDictionary*)option callback:(CompletionBlock)callback {
-    NSMutableDictionary *param = [@{@"title":title} mutableCopy];
-    for (id key in [option keyEnumerator]) {
-        param[key] = [option valueForKey:key];
-    }
-    
+- (void)addBook:(NSString*)title DEFAULT_PARAM {
+    MAKE_PARAM(@{@"title":title});
     [self POST:BOOK_URL parameters:param DEFAULT_CALLBACK];
 }
 
-- (void)getBook:(int)bookId callback:(CompletionBlock)callback {
-    [self GET:BOOKID_URL(bookId) parameters:nil DEFAULT_CALLBACK];
+- (void)getBook:(int)bookId DEFAULT_PARAM {
+    [self GET:BOOKID_URL(bookId) parameters:option DEFAULT_CALLBACK];
 }
 
-- (void)updateBook:(int)bookId option:(NSDictionary*)option callback:(CompletionBlock)callback {
+- (void)updateBook:(int)bookId DEFAULT_PARAM {
     [self PUT:BOOKID_URL(bookId) parameters:option DEFAULT_CALLBACK];
 }
 
-- (void)deleteBook:(int)bookId option:(NSDictionary*)option callback:(CompletionBlock)callback {
+- (void)deleteBook:(int)bookId DEFAULT_PARAM {
     [self DELETE:BOOKID_URL(bookId) parameters:option DEFAULT_CALLBACK];
 }
 
-- (void)searchBook:(NSDictionary*)option callback:(CompletionBlock)callback {
+- (void)searchBook:DEFAULT_PARAM2 {
     [self GET:BOOK_URL"/search" parameters:option DEFAULT_CALLBACK];
 }
 
@@ -94,43 +106,57 @@ failure:^(NSURLSessionDataTask *task, NSError *error) {\
 
 // === [/bookshelves] Bookshelves API
 
-- (void)getBookshelf:(int)userId option:(NSDictionary*)option callback:(CompletionBlock)callback {
+- (void)getBookshelf:(int)userId DEFAULT_PARAM {
     [self GET:BOOKSHELFID_URL(userId) parameters:option DEFAULT_CALLBACK];
 }
 
-- (void)addBookToBookshelf:(int)userId bookId:(int)bookId option:(NSDictionary*)option callback:(CompletionBlock)callback {
-    NSMutableDictionary *param = [@{@"book_id":[NSNumber numberWithInt:bookId]} mutableCopy];
-    for (id key in [option keyEnumerator]) {
-        param[key] = [option valueForKey:key];
-    }
-    
+- (void)addBookToBookshelf:(int)userId bookId:(int)bookId DEFAULT_PARAM {
+    MAKE_PARAM(@{@"book_id":INT2NS(bookId)});
     [self GET:BOOKSHELFID_URL(userId) parameters:param DEFAULT_CALLBACK];
 }
 
-- (void)updateBookshelf:(int)userId option:(NSDictionary*)option callback:(CompletionBlock)callback {
+- (void)updateBookshelf:(int)userId DEFAULT_PARAM {
     [self PUT:BOOKSHELFID_URL(userId) parameters:option DEFAULT_CALLBACK];
 }
 
-- (void)deleteBookshelf:(int)userId option:(NSDictionary*)option callback:(CompletionBlock)callback {
+- (void)deleteBookshelf:(int)userId DEFAULT_PARAM {
     [self DELETE:BOOKSHELFID_URL(userId) parameters:option DEFAULT_CALLBACK];
 }
 
-- (void)getBookInBookshelf:(int)userId bookId:(int)bookId option:(NSDictionary*)option callback:(CompletionBlock)callback {
+- (void)getBookInBookshelf:(int)userId bookId:(int)bookId DEFAULT_PARAM {
     [self GET:BOOKSHELFIDID_URL(userId, bookId) parameters:option DEFAULT_CALLBACK];
 }
 
-- (void)updateBookInBookshelf:(int)userId bookId:(int)bookId option:(NSDictionary*)option callback:(CompletionBlock)callback {
+- (void)updateBookInBookshelf:(int)userId bookId:(int)bookId DEFAULT_PARAM {
     [self PUT:BOOKSHELFIDID_URL(userId, bookId) parameters:option DEFAULT_CALLBACK];
 }
 
-- (void)deleteBookInBookshelf:(int)userId bookId:(int)bookId option:(NSDictionary*)option callback:(CompletionBlock)callback {
+- (void)deleteBookInBookshelf:(int)userId bookId:(int)bookId DEFAULT_PARAM {
     [self DELETE:BOOKSHELFIDID_URL(userId, bookId) parameters:option DEFAULT_CALLBACK];
 }
 
-- (void)searchBookInBookshelf:(int)userId option:(NSDictionary*)option callback:(CompletionBlock)callback {
+- (void)searchBookInBookshelf:(int)userId DEFAULT_PARAM {
     [self GET:BOOKSHELFID_URL(userId) parameters:option DEFAULT_CALLBACK];
 }
 
 // === [/bookshelves] end
+
+// === [/users/:user_id/borrow] Borrow API
+
+- (void)getBorrow:(int)userId DEFAULT_PARAM {
+    [self GET:BORROW_URL(userId) parameters:option DEFAULT_CALLBACK];
+}
+
+- (void)addBorrow:(int)userId bookId:(int)bookId lenderId:(int)lenderId DEFAULT_PARAM {
+    MAKE_PARAM((@{@"book_id":INT2NS(bookId), @"lender_id":INT2NS(lenderId)}));
+    [self POST:BORROW_URL(userId) parameters:param DEFAULT_CALLBACK];
+}
+
+- (void)deleteBorrow:(int)userId bookId:(int)bookId DEFAULT_PARAM {
+    [self DELETE:BORROWID_URL(userId, bookId) parameters:option DEFAULT_CALLBACK];
+}
+
+// === [/users/:user_id/borrow] end
+
 
 @end
