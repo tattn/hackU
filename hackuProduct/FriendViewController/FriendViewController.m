76c5ccaceab2069d3d11d6de5f029c100b607939
Tatsuya Tanaka <tatsuyars@yahoo.co.jp>
2015-07-26 01:45:48 +0900

    Fixed little bugs

--- hackuProduct/FriendViewController/FriendViewController.m
+++ hackuProduct/FriendViewController/FriendViewController.m
@@ -86,6 +86,9 @@
     if (num > 0) {
         self.navigationController.tabBarItem.badgeValue = [NSString stringWithFormat:@"%d", num];
     }
+    else {
+        self.navigationController.tabBarItem.badgeValue = nil; // nilにすると消える
+    }
 }
 
 - (void)didTapAddFriend:(id)selector {
@@ -111,7 +114,7 @@
     cell.firendNameLabel.text = friend[@"name"];
     
     if ([(NSNumber*)friend[@"new"] isEqual: @YES]) {
-        cell.alpha = 0.5f;
+        cell.backgroundColor = [UIColor yellowColor];
     }
     
     return cell;
@@ -123,30 +126,34 @@
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
     [tableView deselectRowAtIndexPath:indexPath animated:YES];
-    FriendBookShelfCollectionViewController *friendBookShelfCollectionVC = [[FriendBookShelfCollectionViewController alloc] init];
-    
-    [self.navigationController pushViewController:friendBookShelfCollectionVC animated:YES];
+    NSDictionary* friend = _friends[indexPath.row];
+    if ([(NSNumber*)friend[@"new"] isEqual: @NO]) {
+        FriendBookShelfCollectionViewController *friendBookShelfCollectionVC = [[FriendBookShelfCollectionViewController alloc] init];
+        
+        [self.navigationController pushViewController:friendBookShelfCollectionVC animated:YES];
+    }
 }
 
 - (NSArray *)tableView:(UITableView *)tableView editActionsForRowAtIndexPath:(NSIndexPath *)indexPath {
+    // 拒否ボタン
+    UITableViewRowAction *deleteAction = [UITableViewRowAction rowActionWithStyle:UITableViewRowActionStyleDestructive title:@"拒否"
+            handler:^(UITableViewRowAction *action, NSIndexPath *indexPath) {
+                NSDictionary* friend = _friends[indexPath.row];
+                [self rejectNewFriendById:((NSNumber*)friend[@"id"]).intValue];
+    }];
+    
+    // 許可ボタン
+    UITableViewRowAction *editAction = [UITableViewRowAction rowActionWithStyle:UITableViewRowActionStyleNormal title:@"許可"
+            handler:^(UITableViewRowAction *action, NSIndexPath *indexPath) {
+                NSDictionary* friend = _friends[indexPath.row];
+                [self allowNewFriendById:((NSNumber*)friend[@"id"]).intValue];
+    }];
+    return @[deleteAction, editAction];
+}
+
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
     NSDictionary* friend = _friends[indexPath.row];
-    if ([(NSNumber*)friend[@"new"] isEqual: @YES]) {
-        // 拒否ボタン
-        UITableViewRowAction *deleteAction = [UITableViewRowAction rowActionWithStyle:UITableViewRowActionStyleDestructive title:@"拒否"
-                handler:^(UITableViewRowAction *action, NSIndexPath *indexPath) {
-                    NSDictionary* friend = _friends[indexPath.row];
-                    [self rejectNewFriendById:((NSNumber*)friend[@"id"]).intValue];
-        }];
-        
-        // 許可ボタン
-        UITableViewRowAction *editAction = [UITableViewRowAction rowActionWithStyle:UITableViewRowActionStyleNormal title:@"許可"
-                handler:^(UITableViewRowAction *action, NSIndexPath *indexPath) {
-                    NSDictionary* friend = _friends[indexPath.row];
-                    [self allowNewFriendById:((NSNumber*)friend[@"id"]).intValue];
-        }];
-        return @[deleteAction, editAction];
-    }
-    return @[];
+    return ((NSNumber*)friend[@"new"]).intValue;
 }
 
 - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
