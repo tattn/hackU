0bb032476ffc2cb47e72252c86e446f8de4657d1
Tatsuya Tanaka <tatsuyars@yahoo.co.jp>
2015-07-29 06:07:31 +0900

    Add SNS class for Line

---
+++ hackuProduct/SNS.h
@@ -0,0 +1,18 @@
+//
+//  SNS.h
+//  hackuProduct
+//
+//  Created by Tanaka Tatsuya on 2015/07/29.
+//  Copyright (c) 2015年 Tatsuya Tanaka. All rights reserved.
+//
+
+#ifndef hackuProduct_SNS_h
+#define hackuProduct_SNS_h
+
+@interface SNS : NSObject
+
++ (void)postToLine:(NSString*)message;
+
+@end
+
+#endif
